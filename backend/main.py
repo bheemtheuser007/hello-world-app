9e5ba15a152779cbc3603f59072fda385667050d
bheemtheuser007 <youcansurelydoit@gmail.com>
2026-05-01 14:30:09 +0000

    backend-Test

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"message": "Hello from the Backend!", "service": "backend"}
+    return {"message": "Hello from the Backend - Harness!", "service": "backend"}
 
 @app.get("/health")
 def health():
